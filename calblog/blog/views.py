43f7358f21c342eb4c1a11b69025f028d58c53ea
Caleb <calebnartey78@gmail.com>
2025-09-06 02:46:57 +0000

    Implement PostList and PostDetail views for blog posts

--- calblog/blog/views.py
+++ calblog/blog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Post
+from django.views import generic
+from django.views.decorators.http import require_GET
+from django.http import HttpResponse
 
-# Create your views here.
+class PostList(generic.ListView):
+  queryset=Post.objects.filter(status=1).order_by('-created_on')
+  template_name='index.html'
+  paginate_by = 4
+
+class PostDetail(generic.DetailView):
+  model=Post
+  template_name='post.html'

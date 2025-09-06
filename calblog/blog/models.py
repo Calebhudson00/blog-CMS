7f3ac8984b92d5270d7b61b00258d4a779e58122
Caleb <calebnartey78@gmail.com>
2025-09-06 01:35:41 +0000

    created models to migrate the database

--- calblog/blog/models.py
+++ calblog/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = ((0, "Draft"), 
+          (1, "Published"), 
+          (2, "Delete"))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    metades = models.CharField(max_length=300, default="new post")
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
